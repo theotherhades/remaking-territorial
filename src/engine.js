119bb12a2382a3ca20e2e205bcb9970e3f48dbbc
theotherhades <64885838+theotherhades@users.noreply.github.com>
2023-01-30 21:25:38 +1300

    Lots of stuff: rewrote drawing system for multiple nations, changed appearance slightly, changed shape of nations to be the same as actual territorial

--- src/engine.js
+++ src/engine.js
@@ -8,9 +8,13 @@ export default class GameEngine {
         this.borderPixels = new Set([]);
         this.debugMode = false;
 
-        document.addEventListener("keyup", (event) => {
+        // `nation` will be the internal name for player territory
+        this.nations = {};
+        this.player = "test";
+
+        document.addEventListener("keydown", (event) => {
             if (event.code === "Space") {
-                this.expandPixels();
+                this.expandPixels(this.player);
             }
         });
     }
@@ -33,32 +37,67 @@ export default class GameEngine {
      * @param {*} x
      * @param {*} y
      */
-    drawPixel(x, y, init = false) {
+    drawPixel(nation, x, y, init = false) {
         if (this.checkInt(x) === false || this.checkInt(y) === false) {
             console.error(`Pixels must only be drawn at coordinates divisible by 4\nProvided coordinates: (x:${x}, y:${y})`);
         } else {
-            this.ctx.fillStyle = "black";
-            this.ctx.fillRect(x, y, 4, 4);
-            this.pixelsOwned.add(`${x},${y}`);
-            if (init) {
-                this.borderPixels.add(`${x},${y}`);
+            let taken = false;
+            for (const nation of Object.keys(this.nations)) {
+                if (this.nations[nation].pixelsOwned.has(`${x},${y}`)) {
+                    taken = true;
+                    break;
+                }
+            }
+            if (!taken) {
+                this.ctx.fillStyle = this.nations[nation].color;
+                this.ctx.fillRect(x, y, 4, 4);
+                this.nations[nation].pixelsOwned.add(`${x},${y}`);
+                if (init) {
+                    this.nations[nation].borderPixels.add(`${x},${y}`);
+                }
             }
         }
     }
 
     /**
-     * Checks if pixel (x, y) is owned.
-     * @param {*} x 
-     * @param {*} y 
+     * Register a new nation with the engine, for multiplayer.
+     * @param {*} id The internal id for the nation.
+     * @param {*} color The color the nation's pixels will be.
+     * @param {*} isPlayer Does this nation belong to the client's player? (true or false)
+     * @param {*} x
+     * @param {*} y
      */
-    hasPixel(x, y) {
-        return this.pixelsOwned.has(`${x},${y}`);
+    registerNation(id, color, isPlayer, x, y) {
+        this.nations[id] = {
+            color: color,
+            isPlayer: isPlayer,
+            pixelsOwned: new Set([]),
+            borderPixels: new Set([])
+        };
+        this.player = id;
+        for (const pixel of [
+            [x, y],
+            [x + 4, y],
+            [x, y + 4],
+            [x + 4, y + 4],
+            [x - 4, y],
+            [x, y - 4],
+            [x + 8, y],
+            [x, y + 8],
+            [x - 4, y + 4],
+            [x + 4, y - 4],
+            [x + 8, y + 4],
+            [x, y + 8],
+            [x + 4, y + 8]
+        ]) {
+            this.drawPixel(id, pixel[0], pixel[1], true);
+        }
     }
 
-    expandPixels() {
+    expandPixels(nation) {
         let pixelsToOccupy = new Set([]);
 
-        for (let pixel of this.borderPixels) {
+        for (let pixel of this.nations[nation].borderPixels) {
             pixel = pixel.split(",");
             for (const neighbor of [
                 [parseInt(pixel[0]) - 4, parseInt(pixel[1])], // West
@@ -67,21 +106,23 @@ export default class GameEngine {
                 [parseInt(pixel[0]) + 4, parseInt(pixel[1])], // East
             ]) {
                 if (!(neighbor[0] < 0 || neighbor[0] > this.canvasWidth || neighbor[1] < 0 || neighbor[1] > this.canvasHeight)) {
-                    if (this.hasPixel(neighbor[0], neighbor[1])) {
+                    if (this.nations[nation].pixelsOwned.has(`${neighbor[0]},${neighbor[1]}`)) {
                         if (this.debugMode) { console.log("pixel owned"); }
                         continue;
                     } else {
                         pixelsToOccupy.add(`${neighbor[0]},${neighbor[1]}`);
                     }
+                } else if (this.debugMode) {
+                    console.log("pixel is out of bounds");
                 }
             }
         }
         for (let pixel of pixelsToOccupy) {
             pixel = pixel.split(",");
-            this.drawPixel(pixel[0], pixel[1]);
+            this.drawPixel(nation, pixel[0], pixel[1]);
         }
-        if (this.debugMode) { console.log(this.borderPixels); }
-        this.borderPixels = new Set(pixelsToOccupy);
-        if (this.debugMode) { console.log(this.borderPixels); }
+        if (this.debugMode) { console.log(this.nations[nation].borderPixels); }
+        this.nations[nation].borderPixels = new Set(pixelsToOccupy);
+        if (this.debugMode) { console.log(this.nations[nation].borderPixels); }
     }
 }
--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import GameEngine from "./engine.js";
 const engine = new GameEngine("gameCanvas");
 window.engine = engine;
 
-engine.drawPixel(100, 100, true);
+engine.debugMode = true;
+engine.registerNation("hi", "orange", true, 100, 100);
+engine.registerNation("hmm", "blue", true, 200, 200);
